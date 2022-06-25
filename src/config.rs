ef6e83831a5241a06b09e471948dc9321ff06e88
IFcoltransG <47414286+IFcoltransG@users.noreply.github.com>
2022-06-25 19:17:50 +1200

    Make separate folder for shaders

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, about, long_about = None)]
 pub(crate) struct Config {
-    #[clap(short, long, value_parser, default_value = "./src/shader.wgsl")]
+    #[clap(short, long, value_parser, default_value = "./shaders/shader.wgsl")]
     pub path: String,
 }
